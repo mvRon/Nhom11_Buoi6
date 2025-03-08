19aa7f7ba7712ef6f284403a07fa0ea43aeb00b8
mvRon <phatvan12388@gmail.com>
2025-03-08 10:01:12 +0700

    Update

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 })
+
+//Connection
+
+const mongoURI = "mongodb+srv://ronm250304:RoN13141516@cluster0.xn5xe.mongodb.net/"
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected successfilly")
+}).catch((error) => {
+    console.log("MongoDB connection error", error)
+})
